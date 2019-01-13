dc68d7dbd76b0bbf41f0802f64754f8042240fcd
Fernando Paladini <fnpaladini@gmail.com>
2019-01-13 21:04:04 -0200

    Add tests.

--- django-truck-load/truck_load/trucks/tests/test_models.py
+++ django-truck-load/truck_load/trucks/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from ..models import Truck
+from ..models import Load
 
 class TruckTest(TestCase):
     """ Test module for Truck model """
@@ -17,3 +18,40 @@ class TruckTest(TestCase):
             truck1.get_truck_info(), "Hartford Plastics Incartford is located in Florence / AL at latitude 34.799810 and longitude -87.677251")
         self.assertEqual(
             truck2.get_truck_info(), "Beyond Landscape & Design Llcilsonville is located in Freemont / CA at latitude 37.548270 and longitude -121.988572")
+
+class LoadTest(TestCase):
+    """ Test module for Load model """
+
+    def setUp(self):
+        Load.objects.create(
+            id_load=1, 
+            product='Light bulbs', 
+            orig_city='Sikeston', 
+            orig_state='MO',
+            orig_lat=36.876719,
+            orig_lng=-89.5878579,
+            dest_city='Grapevine',
+            dest_state='TX',
+            dest_lat=32.9342919,
+            dest_lng=-97.0780654
+        )
+        Load.objects.create(
+            id_load=2, 
+            product='Recyclables', 
+            orig_city='Christiansburg', 
+            orig_state='VA',
+            orig_lat=37.1298517,
+            orig_lng=-80.4089389,
+            dest_city='Apopka',
+            dest_state='FL',
+            dest_lat=28.6934076,
+            dest_lng=-81.5322149
+        )
+
+    def test_truck_name(self):
+        obj1 = Load.objects.get(product='Light bulbs')
+        obj2 = Load.objects.get(product='Recyclables')
+        self.assertEqual(
+            obj1.get_load_info(), "Light bulbs is located in Sikeston / MO and should travel to Grapevine / TX.")
+        self.assertEqual(
+            obj2.get_load_info(), "Recyclables is located in Christiansburg / VA and should travel to Apopka / FL.")
